0ecb069f6b883cb353295b5b4426c5479a063e72
linguini1 <matteo.golin@gmail.com>
2023-03-07 12:02:07 -0500

    Removed the writewrow function in favour for a faster append method that modifies the original dataframe. Whenever an error occurrs now, the DataFrame is still filtered and saved. Input is also taken for the filename to prevent over-writing.

--- collection/main.py
+++ collection/main.py
@@ -4,7 +4,7 @@ __author__ = "Matteo Golin"
 # Imports
 import ratemyprofessor
 from ratemyprofessor import School, Professor
-from query import load_queries
+from query import load_queries, Query
 import pandas as pd
 
 # Constants
@@ -14,18 +14,26 @@ COLUMNS: list[str] = ["school", "professor", "course", "comment"]
 MINIMUM_CHARS: int = 5
 
 
-# Helper functions
-def write_row(row: list[str], df: pd.DataFrame) -> pd.DataFrame:
-    """Returns the DataFrame with the given row appended to it."""
+def scrape_queries(queries: list[Query], df: pd.DataFrame, log: bool = True) -> None:
+    """Adds the results of scraping the queries to a Pandas DataFrame."""
 
-    # Label the data for Pandas
-    new_row = {}
-    for label, value in zip(COLUMNS, row):
-        new_row[label] = value
+    for query in queries:
+        school: School = ratemyprofessor.get_school_by_name(query.school)  # Get the school from RateMyProf
 
-    # Add to the dataframe
-    new_row = pd.Series(new_row).to_frame().T
-    return pd.concat([df, new_row], ignore_index=True)
+        # Get all the professors from RateMyProf
+        for professor_name in query.professors:
+            professor: Professor = ratemyprofessor.get_professor_by_school_and_name(school, professor_name)
+
+            # Get all the ratings for each course
+            for course in professor.courses:
+
+                # Print for each course to show progress
+                if log:
+                    print(f"{query.school}: {professor_name} - {course.name}")
+
+                # Add each rating to the DataFrame
+                for rating in professor.get_ratings(course_name=course.name):
+                    df.loc[df.shape[0]] = [school.name, professor.name, course.name, rating.comment]
 
 
 # Main
@@ -37,27 +45,22 @@ def main():
     # Set up dataset
     dataset = pd.DataFrame(columns=COLUMNS)
 
+    # Get desired filename
+    filename = input("File name for the CSV output: ")
+
     # Complete all query searches
-    for query in queries:
-        school: School = ratemyprofessor.get_school_by_name(query.school)  # Get the school from RateMyProf
-
-        # Get all the professors from RateMyProf
-        for professor_name in query.professors:
-            professor: Professor = ratemyprofessor.get_professor_by_school_and_name(school, professor_name)
-
-            # Get all the ratings for each course
-            for course in professor.courses:
-                print(f"{query.school}: {professor_name} - {course.name}")  # Print for each course to show progress
-                for rating in professor.get_ratings(course_name=course.name):
-                    # Add each rating to the DataFrame
-                    dataset = write_row([school.name, professor.name, course.name, rating.comment], dataset)
+    try:
+        scrape_queries(queries, dataset, log=True)  # Show progress in console
+    except (Exception, KeyboardInterrupt):
+        # If there is an error, save the current progress
+        print(dataset)
 
     # Remove bad data
     dataset = dataset[dataset["comment"].str.len() > MINIMUM_CHARS]  # Can't have too few characters
     dataset = dataset[dataset["comment"].str.startswith("No Comments") == False]  # Removes empty comments
 
     # Write the DataFrame to a CSV file
-    dataset.to_csv(EXPORT_FILE, index=False)
+    dataset.to_csv(f"./{filename}.csv", index=False)
 
 
 if __name__ == "__main__":
