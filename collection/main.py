e31e8d1566d1a115e61b03d72978c88a8ed5466e
Matteo Golin <matteo.golin@gmail.com>
2023-03-06 22:18:20 -0500

    Data can now be written to CSV files!!

--- collection/main.py
+++ collection/main.py
@@ -6,11 +6,12 @@ import ratemyprofessor
 from ratemyprofessor import School, Professor
 from query import load_queries
 import pandas as pd
-import logging
 
 # Constants
 QUERY_FILE: str = "./queries.json"
+EXPORT_FILE: str = "./ratings.csv"
 COLUMNS: list[str] = ["school", "professor", "course", "comment"]
+MINIMUM_CHARS: int = 5
 
 
 # Helper functions
@@ -36,9 +37,6 @@ def main():
     # Set up dataset
     dataset = pd.DataFrame(columns=COLUMNS)
 
-    # Set up logging
-    logging.basicConfig(level=logging.INFO)
-
     # Complete all query searches
     for query in queries:
         school: School = ratemyprofessor.get_school_by_name(query.school)  # Get the school from RateMyProf
@@ -46,14 +44,20 @@ def main():
         # Get all the professors from RateMyProf
         for professor_name in query.professors:
             professor: Professor = ratemyprofessor.get_professor_by_school_and_name(school, professor_name)
-            logging.info(f"{query.school}: {professor_name}")
 
             # Get all the ratings for each course
             for course in professor.courses:
+                print(f"{query.school}: {professor_name} - {course.name}")  # Print for each course to show progress
                 for rating in professor.get_ratings(course_name=course.name):
+                    # Add each rating to the DataFrame
                     dataset = write_row([school.name, professor.name, course.name, rating.comment], dataset)
 
-    print(dataset)
+    # Remove bad data
+    dataset = dataset[dataset["comment"].str.len() > MINIMUM_CHARS]  # Can't have too few characters
+    dataset = dataset[dataset["comment"].str.startswith("No Comments") == False]  # Removes empty comments
+
+    # Write the DataFrame to a CSV file
+    dataset.to_csv(EXPORT_FILE)
 
 
 if __name__ == "__main__":
