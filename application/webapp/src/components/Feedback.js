814a1a6e0b0c9bb78ecc725747cbd03d21572a6f
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2023-03-18 18:26:31 -0400

    Update main page to show dropdown menu and submit button

--- application/webapp/src/components/Feedback.js
+++ application/webapp/src/components/Feedback.js
@@ -1,28 +1,24 @@
-import React from 'react';
+import React from "react";
 import "./Feedback.css";
 
-const courses = [
-  "Course A",
-  "Course B",
-  "Course C",
-  "Course D",
-  "Course E",
-];
+const courses = ["Course A", "Course B", "Course C", "Course D", "Course E"];
 
 const Feedback = () => {
   return (
-    <div className="feedback-container">
-      <h1>Feedback</h1>
-      <div className="form-container">
-        <label htmlFor="course-select">Course Name:</label>
-        <select id="course-select">
-          {courses.map((course) => (
-            <option key={course} value={course}>
-              {course}
-            </option>
-          ))}
-        </select>
-        <button type="submit">Submit</button>
+    <div className="container">
+      <label htmlFor="course-select">Course Name:</label>
+      <select id="course-select">
+        {courses.map((course) => (
+          <option key={course} value={course}>
+            {course}
+          </option>
+        ))}
+      </select>
+
+      <div className="button-container">
+        <button className="submitButton" type="submit">
+          Submit
+        </button>
       </div>
     </div>
   );
