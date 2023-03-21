79b7fe6a947bcecb42789ea608794e91d5bf11b6
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2023-03-21 09:59:20 -0400

    Update results  to properly link with other pages

---
+++ application/webapp/src/pages/Results.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Results.css";
+
+const courses = [
+  "Course A",
+  "Course B",
+  "Course C",
+  "Course D",
+  "Course E",
+];
+
+export default function Results(){
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
