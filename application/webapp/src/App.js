5fdd3a17bd54f8c60a7901d9be8de012d5f95acd
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2023-03-21 09:57:11 -0400

    Update webapp to properly route between pages

--- application/webapp/src/App.js
+++ application/webapp/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import "./App.css";
+import React from "react";
+
+//Pages
+import Feedback from "./pages/Feedback";
+import Results from "./pages/Results";
+
+//Components
+import {Routes, Route} from "react-router-dom"
 import Navbar from './components/Navbar';
-import Feedback from './components/Feedback'
+import PageLink from "./components/PageLink";
 
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    <Feedback />
+    <div id="App">
+      <Navbar>
+        <PageLink to="/">Feedback</PageLink>
+        <PageLink to="/results">Results</PageLink>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Feedback />}></Route>
+        <Route path="/results" element={<Results />}></Route>
+      </Routes>
     </div>
     
   );
