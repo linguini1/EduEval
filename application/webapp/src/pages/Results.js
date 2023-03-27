08ec35af52dbf5bcad4a808f3e37182a22d11e66
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2023-03-27 10:57:50 -0400

    Update results page to show feedback boxes and course names

--- application/webapp/src/pages/Results.js
+++ application/webapp/src/pages/Results.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./Results.css";
 
+const courses = ["Course A", "Course B", "Course C", "Course D", "Course E"];
+const positiveFeedback = "This is the positive feedback";
+const negativeFeedback = "This is the negative feedback";
+
 export default function Results(){
   return (
-    <div>
+    <div className="container">
+      <label htmlFor="course-select">Course Name:</label>
+      <select id="course-select">
+        {courses.map((course) => (
+          <option key={course} value={course}>
+            {course}
+          </option>
+        ))}
+      </select>
       
+      <div className="feedback-box pos">
+          <textarea readonly value={positiveFeedback}></textarea>
+      </div>
+      <div className="feedback-box neg">
+          <textarea readonly value={negativeFeedback}></textarea>
+      </div>
     </div>
   );
 };
