8fa41c2d356bf0932e0384c088952c603d8440af
Hamnah <hamnahq25@gmail.com>
2023-04-09 01:14:15 -0400

    Set up Flask api, set upload button to only accept csv, create post method that sends csv file and saves it as a parquet.gzip file.

--- application/webapp/src/pages/Feedback.js
+++ application/webapp/src/pages/Feedback.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
 import "./Feedback.css";
 
 const courses = ["Course A", "Course B", "Course C", "Course D", "Course E"];
 
 export default function Feedback() {
+
+  const [file, setFile] = useState(null);
+
   const handleFileUpload = (event) => {
-    const file = event.target.files[0];
-    // do something with the uploaded file
+    // Checks if uploaded file is correct type
+    if (event.target.files[0].type !== 'text/csv'){
+        console.log('Invalid file type. Please select a CSV file.');
+        return ;
+    }
+    setFile(event.target.files[0]); 
   }
+
+
+  async function handleSubmit (event) {
+
+    if (file != null){
+      event.preventDefault(); // Cancels the default action of the event
+      const formData = new FormData(); //Creates FormData to easily send file as part of HTTP request
+      formData.append('file', file);
+
+      //Send the file to the server
+      const response = await fetch('http://localhost:5000/upload', {
+        method: 'POST',
+        body: formData
+      })
+
+        .then((response) => {
+          console.log('File uploaded successfully!');
+        })
+        .catch((error) => {
+          console.error('Error uploading file:', error);
+        });
+    }    
+  };
+
 
   return (
     <div className="container">
@@ -22,11 +54,11 @@ export default function Feedback() {
 
       <div >
         <label htmlFor="upload-file" className="upload-file">Upload Review File:</label> 
-        <input type="file" id="upload-file" name="upload-file" onChange={handleFileUpload}/>
+        <input type="file" id="upload-file" name="upload-file" onChange={handleFileUpload} accept= ".csv"/>
       </div>
 
       <div className="button-container">
-        <button className="submitButton" type="submit">
+        <button className="submitButton" type="submit" onClick = {handleSubmit}>
           Submit
         </button>
       </div>
---
+++ training/server.py
@@ -0,0 +1,26 @@
+#Imports
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import pandas as pd
+
+
+app = Flask(__name__) #Creates a Flask applicaton 
+cors = CORS(app) #Enable CORS 
+app.config['CORS_HEADERS'] = 'Content-Type' # Allow the 'Content-Type' header in CORS requests
+
+
+
+#API route for uploading the file
+@app.route('/upload', methods=['POST'])
+@cross_origin(origin='localhost:3000')
+def upload ():
+    file = request.files['file']
+    #file.save('data/userdata.csv') #if we want to save the csv file
+    df = pd.read_csv(file)
+    df.to_parquet('data/data_file.parquet.gzip')
+    return 'File uploaded successfully'
+
+
+if __name__ == '__main__':
+    # Run API on port 5000
+    app.run(debug=True, port= 5000)
